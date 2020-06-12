87fc2ab59a4ce9789fe5f3ddd27ee365bbd74e4a
fukutaro1120 <bicyclelife.tk@gmail.com>
2020-06-12 17:35:57 +0900

    メイン背景画像修正

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -2,60 +2,29 @@
 
 @section('content')
 
-indexです<br>
-ログイン後のホーム画面を表示（マイページアイコンがある）
+<!-- indexです<br>
+ログイン後のホーム画面を表示（マイページアイコンがある） -->
 
-<div class="jumbotron jumbotron-fluid text-center">
-      <h3 class="display-3">飲musubi</h3>
-        <p class="lead">一杯の酒と人を結ぶ</p>
-</div> 
 
-    <!-- <div class="card-deck m-3">
-      <div class="card">
-        <a href="detail.html">
-          <img src="/storage/img/sato10.jpg"class="card-img-top" alt="...">
-        </a>
-        <div class="card-body">
-          <h3 class="card-title h4">
-            <a href="detail.html" class="text-dark">海鮮居酒屋里</a>
-          </h3>
-          <h6 class="card-subtitle text-secondary">居酒屋</h6>
-        </div>
-    </div>
-    <div class="card">
-        <a href="detail.html">
-          <img src="/storage/img/sato10.jpg" class="card-img-top" alt="...">
-        </a>
-        <div class="card-body">
-          <h3 class="card-title h4">
-            <a href="detail.html" class="text-dark">海鮮居酒屋里</a>
-          </h3>
-          <h6 class="card-subtitle text-secondary">居酒屋</h6>
-        </div>
-    </div>
-    <div class="card">
-        <a href="detail.html">
-          <img src="/storage/img/sato10.jpg" class="card-img-top" alt="...">
-        </a>
-        <div class="card-body">
-          <h3 class="card-title h4">
-            <a href="detail.html" class="text-dark">海鮮居酒屋里</a>
-          </h3>
-          <h6 class="card-subtitle text-secondary">居酒屋</h6>
-        </div>
-    </div>  
-  </div> -->
+  
+<div class="jumbotron jumbotron-fluid text-center p-0">
+  <div class="bg-rgba pt-5">
+              <h3 class="display-3 pt-5">飲musubi</h3>
+                <p class="lead">一杯の酒と人を結ぶ</p>
+      </div> 
+  </div> 
+            
 
-  <!-- <div class="jumbotron jumbotron-fluid text-center">
-          <h3 class="display-3">飲musubiとは</h3>
+  <div class="container text-center">
+          <h3 class="display-3">飲musubi</h3>
           <p class="lead">ここに説明</p>
           <p class="lead">ここに説明</p>
           <p class="lead">ここに説明</p>
-  </div>  -->
+  </div> 
 
   <footer class="footer mt-auto  py-3 bg-light">
-      <div class="container">
-         <span class="text-muted">Copyright(c) </span>
+      <div class="container text-center">
+         <span class="text-muted">Copyright(c) 飲musubi</span>
       </div>
   </footer>
 
--- resources/views/mypage/show.blade.php
+++ resources/views/mypage/show.blade.php
@@ -9,27 +9,27 @@ showです<br>
 <div class="container">
   <div class="row">
       @foreach($shops as $shop)
-            <div class="card col-md-4 shadow mb-5 ">
-                <img src="{{ asset('storage/image/'.$shop->image_path) }}" class="img-fluid  d-block w-100">
+            <div class="card col-md-3 shadow mb-5 ">
+                <img src="{{ asset('storage/image/'.$shop->image_path) }}" class="img-fluid d-block w-100 ">
                   <div class="card-body">
 
                       <div class="container mb-3">
                           <div class="row">
-                              <div class="col-4">
+                              <div class="col-6">
                                 <h6>店名:</h6>
                               </div>
-                              <div class="col-8">
-                                <h5 class="card-title">{{ $shop->shop_name }}</h5>
+                              <div class="col-6">
+                                <h6 class="card-title">{{ $shop->shop_name }}</h6>
                               </div>
                           </div>
                       </div>
 
                       <div class="container mb-3">
                           <div class="row">
-                              <div class="col-4">
+                              <div class="col-6">
                                 <h6>ジャンル:</h6>
                               </div>
-                              <div class="col-8">
+                              <div class="col-6">
                                 <h6 class="card-title">{{ $shop->category }}</h6>
                               </div>
                           </div>
@@ -37,10 +37,10 @@ showです<br>
 
                       <div class="container mb-4">
                           <div class="row">
-                              <div class="col-4">
+                              <div class="col-6">
                                 <h6>住所:</h6>
                               </div>
-                              <div class="col-8">
+                              <div class="col-6">
                                 <h6 class="card-title">{{ $shop->address }}</h6>
                               </div>
                           </div>
