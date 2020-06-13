05abca8e90efbc18dd92317ea8d39500925136ec
fukutaro1120 <bicyclelife.tk@gmail.com>
2020-06-13 17:29:25 +0900

    ロゴ修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{'storage/img/musubi.jpg'}}" width="50" height="40" alt="" loading="lazy">
+                <img src="/storage/img/musubi.jpg" width="50" height="40" alt="" loading="lazy">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
