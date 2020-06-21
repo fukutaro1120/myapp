238f64de444067ebc3c370773b3dfa3d1af660ca
fukutaro1120 <bicyclelife.tk@gmail.com>
2020-06-21 12:39:23 +0900

    詳細画面の調整

--- resources/views/mypage/edit.blade.php
+++ resources/views/mypage/edit.blade.php
@@ -2,96 +2,98 @@
 
 @section('content')
 
-<!-- ここから調整 -->
-
 <br>
 <br>
 
 <div class="container  mt-2">
   <div class="row text-center">
     <div class="col">
-      <h2>『行ってみたいと思ったら、マイページに追加しよう！』</h2>
+      <h2>『{{ $shop->shop_name}}の紹介ページ』</h2>
     </div>
   </div>
 </div>
 
-<div class="container1 my-3  p-5 bg-white border">
-  <h3>飲musubiメモ</h3>
-  <section class="alternate-layout alternate-layout--reverse">
-    <div class="alternate-layout__img">
-      <img src="{{ asset('storage/image/'.$shop->image_path) }}" class=" d-block w-80" alt="...">
-    </div>
-    <div class="alternate-layout__detail">
-          <h2 class="alternate-layout__heading"> 『{{ $shop->shop_name}}』</h2>
-          <p>週末に行ってみよう</p>
-          <p>料理うまそうだったなー</p>
-    </div>
-  </section>
-
-  <section class="alternate-layout">
-    <div class="alternate-layout__img"> 
-      <img src="{{ asset('storage/image/'.$shop->image_path1) }}" class=" d-block w-80" alt="...">
-    </div>
-    <div class="alternate-layout__detail">
-          <h2 class="alternate-layout__heading">はやく会社終わらないかなー</h2>
-          <p>今日は久しぶに飲みにいける！</p>
-          <p>この店にいくの久しぶりだ！</p>
-    </div>
-  </section>
-  </div>
-        
-
-
-<!-- ここまで -->
-
-<div class="container mt-2 pt-5">
+<!-- 拡大画像ここから -->
+<div class="container mt-2 pt-3">
     <div class="row">
-
-      <div class="col-md-3">
-          <div class="card mb-3">
-            <img src="{{ asset('storage/image/'.$shop->image_path) }}" class=" d-block w-100" alt="..."> 
-          </div>
+      <div class="col-md-3 ">
+         <a href="{{ asset('storage/image/'.$shop->image_path) }}" data-lightbox="group">
+          <img src="{{ asset('storage/image/'.$shop->image_path) }}" width="300">
+        </a>
       </div>
 
-      <div class="col-md-3">
-          <div class="card mb-3">
-            <img src="{{ asset('storage/image/'.$shop->image_path1) }}" class=" d-block w-100" alt="...">
-          </div>
+      <div class="col-md-3 ">
+      <a href="{{ asset('storage/image/'.$shop->image_path1) }}" data-lightbox="group">
+         <img src="{{ asset('storage/image/'.$shop->image_path1) }}" width="300">
+       </a>
       </div>
 
-      <div class="col-md-3">
-          <div class="card mb-3">
-            <img src="{{ asset('storage/image/'.$shop->image_path2) }}" class=" d-block w-100"alt="...">
-          </div>
+      <div class="col-md-3 ">
+      <a href="{{ asset('storage/image/'.$shop->image_path2) }}" data-lightbox="group">
+      <img src="{{ asset('storage/image/'.$shop->image_path2) }}" width="300">
+      </a>
       </div>
 
-      <div class="col-md-3">
-          <div class="card mb-3" >
-            <img src="{{ asset('storage/image/'.$shop->image_path3) }}" class=" d-block w-100" alt="...">
-          </div>
+      <div class="col-md-3 ">
+      <a href="{{ asset('storage/image/'.$shop->image_path3) }}" data-lightbox="group">
+      <img src="{{ asset('storage/image/'.$shop->image_path3) }}" width="300">
+      </a>
       </div> 
     </div>
 </div>
+<!-- 拡大画像ここまで -->
 
-  <div class="container text-center pb-4">  
-          <div class="row">
-              <dt class="col-md-3 ">店舗名</dt>
-              <dd class="col-md-9 ">{{ $shop->shop_name}}</dd>
-              <dt class="col-sm-3">住所</dt>
-              <dd class="col-sm-9">{{ $shop->address}}</dd>
-              <dt class="col-sm-3">ジャンル</dt>
-              <dd class="col-sm-9">{{ $shop->category}}</dd>            
-              <dt class="col-sm-3">おすすめ料理</dt>
-              <dd class="col-sm-9">{{ $shop->recommend}}</dd>
-              <dt class="col-sm-3">お店の作り</dt>
-          </div>
+
+<!-- ここから -->
+
+<div class="container1 my-3  p-5 bg-white border">
+  <h3>飲musubiメモ</h3>
+    <section class="alternate-layout alternate-layout--reverse">
+        <div class="alternate-layout__img">
+          <img src="{{ asset('storage/image/'.$shop->image_path) }}" class=" d-block w-80" alt="...">
+        </div>
+        <div class="alternate-layout__detail">
+              <h2 class="alternate-layout__heading"> 『{{ $shop->shop_name}}』</h2>
+              <p>週末に行ってみよう</p>
+              <p>料理うまそうだったなー</p>
+        </div>
+    </section>
+
+    <section class="alternate-layout">
+        <div class="alternate-layout__img"> 
+          <img src="{{ asset('storage/image/'.$shop->image_path1) }}" class=" d-block w-80" alt="...">
+        </div>
+        <div class="alternate-layout__detail">
+              <h2 class="alternate-layout__heading">はやく会社終わらないかなー</h2>
+              <p>今日は久しぶに飲みにいける！</p>
+              <p>この店にいくの久しぶりだ！</p>
+        </div>
+    </section>
+</div>
+<!-- ここまで -->
+
+<!-- 店詳細 -->
+<div class="container my-3">
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-8">
+      <ul class="list-group list-group-flush">
+          <li class="list-group-item">店名：{{ $shop->shop_name}}</li>
+          <li class="list-group-item">住所：{{ $shop->address}}</li>
+          <li class="list-group-item">ジャンル：{{ $shop->category}}</li>
+          <li class="list-group-item">おすすめ：{{ $shop->recommend}}</li>
+      </ul>
+    </div>
+    <div class="col-2"></div>
   </div>
+</div>
+<!-- 詳細ここまで -->
 
+<!-- マイページ追加ボタン -->
 <div class="container">
     <div class="row">
-      <div class="col-7"></div>
+      <div class="col-9"></div>
         <div class="col--2">
-    <!-- マイページへの追加 -->
             <form action="{{ action('MyPageController@mypageup') }}" method="post" enctype="multipart/form-data">
                 @csrf
                 {{ csrf_field() }}
@@ -99,16 +101,14 @@
                 <input type="submit" class="btn btn-primary" value="マイページに追加">
             </form>
         </div>
-    <!-- マイページ追加処理ここまで -->
-
     </div>
   </div>
+  <!-- 追加ボタンここまで -->
 
 
 
   <!-- 以下アコーディオンスライダー用 -->
   <br>
-
   <style>
     .image_size_m{
       width: 450px;
@@ -151,5 +151,12 @@
 
 <link href="{{ asset('css/font.css') }}" rel="stylesheet">
 <link href="{{ asset('css/edit.css') }}" rel="stylesheet">
+
+<!-- 画像拡大の確認用 -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js" type="text/javascript"></script>
+
+
 
 @endsection
