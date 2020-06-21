6203f0a4538d6ba18dd4ecd64399cc639c963184
fukutaro1120 <bicyclelife.tk@gmail.com>
2020-06-21 12:50:25 +0900

    詳細ページの画像拡大機能を実装

--- resources/views/mypage/edit.blade.php
+++ resources/views/mypage/edit.blade.php
@@ -16,30 +16,40 @@
 <!-- 拡大画像ここから -->
 <div class="container mt-2 pt-3">
     <div class="row">
+
       <div class="col-md-3 ">
-         <a href="{{ asset('storage/image/'.$shop->image_path) }}" data-lightbox="group">
-          <img src="{{ asset('storage/image/'.$shop->image_path) }}" width="300">
+        <div class="img_wrap">
+          <a href="{{ asset('storage/image/'.$shop->image_path) }}" data-lightbox="group">
+            <img src="{{ asset('storage/image/'.$shop->image_path) }}" width="300">
+          </a>
+        </div>
+     </div>
+
+     <div class="col-md-3 ">
+        <div class="img_wrap">
+        <a href="{{ asset('storage/image/'.$shop->image_path1) }}" data-lightbox="group">
+          <img src="{{ asset('storage/image/'.$shop->image_path1) }}" width="300">
+        </a>
+        </div>
+     </div>
+
+    <div class="col-md-3 ">
+      <div class="img_wrap">
+        <a href="{{ asset('storage/image/'.$shop->image_path2) }}" data-lightbox="group">
+          <img src="{{ asset('storage/image/'.$shop->image_path2) }}" width="300">
         </a>
       </div>
-
-      <div class="col-md-3 ">
-      <a href="{{ asset('storage/image/'.$shop->image_path1) }}" data-lightbox="group">
-         <img src="{{ asset('storage/image/'.$shop->image_path1) }}" width="300">
-       </a>
-      </div>
-
-      <div class="col-md-3 ">
-      <a href="{{ asset('storage/image/'.$shop->image_path2) }}" data-lightbox="group">
-      <img src="{{ asset('storage/image/'.$shop->image_path2) }}" width="300">
-      </a>
-      </div>
-
-      <div class="col-md-3 ">
-      <a href="{{ asset('storage/image/'.$shop->image_path3) }}" data-lightbox="group">
-      <img src="{{ asset('storage/image/'.$shop->image_path3) }}" width="300">
-      </a>
-      </div> 
     </div>
+
+    <div class="col-md-3 ">
+      <div class="img_wrap">
+        <a href="{{ asset('storage/image/'.$shop->image_path3) }}" data-lightbox="group">
+          <img src="{{ asset('storage/image/'.$shop->image_path3) }}" width="300">
+        </a>
+      </div> 
+    </div> 
+
+  </div>
 </div>
 <!-- 拡大画像ここまで -->
 
@@ -78,10 +88,10 @@
     <div class="col-2"></div>
     <div class="col-8">
       <ul class="list-group list-group-flush">
-          <li class="list-group-item">店名：{{ $shop->shop_name}}</li>
-          <li class="list-group-item">住所：{{ $shop->address}}</li>
-          <li class="list-group-item">ジャンル：{{ $shop->category}}</li>
-          <li class="list-group-item">おすすめ：{{ $shop->recommend}}</li>
+          <li class="list-group-item">・店名：{{ $shop->shop_name}}</li>
+          <li class="list-group-item">・住所：{{ $shop->address}}</li>
+          <li class="list-group-item">・ジャンル：{{ $shop->category}}</li>
+          <li class="list-group-item">・おすすめ：{{ $shop->recommend}}</li>
       </ul>
     </div>
     <div class="col-2"></div>
@@ -105,49 +115,14 @@
   </div>
   <!-- 追加ボタンここまで -->
 
+<br>
 
-
-  <!-- 以下アコーディオンスライダー用 -->
-  <br>
-  <style>
-    .image_size_m{
-      width: 450px;
-      height: 450px;	
-    }
-  </style>
 
   <link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
   <link rel="stylesheet" type="text/css" href="{{ asset('css/accordion.css') }}">
   <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
   <script src="{{ asset('js/accordion.min.js') }}"></script>
   
-
-<!-- 以下アコーディイン処理 -->
-    <!-- <div class="accordion mb-5">
-          <li><img src="{{ asset('storage/image/'.$shop->image_path) }}" class="image_size_m "></li>
-          <li><img src="{{ asset('storage/image/'.$shop->image_path1) }}" class="image_size_m "></li>
-          <li><img src="{{ asset('storage/image/'.$shop->image_path2) }}" class="image_size_m " ></li>
-          <li><img src="{{ asset('storage/image/'.$shop->image_path3) }}" class="image_size_m " ></li>
-    </div> -->
-  
-
-      <script type="text/javascript">
-          $('.accordion').accordion();
-      </script>
-      <script type="text/javascript">
-
-          var _gaq = _gaq || [];
-          _gaq.push(['_setAccount', 'UA-36251023-1']);
-          _gaq.push(['_setDomainName', 'jqueryscript.net']);
-          _gaq.push(['_trackPageview']);
-
-          (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
-
-      </script>
 
 <link href="{{ asset('css/font.css') }}" rel="stylesheet">
 <link href="{{ asset('css/edit.css') }}" rel="stylesheet">
@@ -156,7 +131,5 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js" type="text/javascript"></script>
-
-
 
 @endsection
